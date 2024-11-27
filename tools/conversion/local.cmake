6cd38b839b8603a98acf79031b812f903b1200f5
Ping-Lin Kuo <ping-lin.kuo@gmail.com>
2024-11-27 19:59:46 +0100

    Modified the cmake files

--- tools/conversion/local.cmake
+++ tools/conversion/local.cmake
@@ -16,17 +16,17 @@ add_executable(csvtobtr ${BTR_CONVERSION_DIR}/csvtobtr.cpp)
 target_include_directories(csvtobtr PRIVATE ${BTR_INCLUDE_DIR})
 target_link_libraries(csvtobtr btrfiles gflags tbb spdlog)
 
-add_executable(btrmeta ${BTR_CONVERSION_DIR}/btrmeta.cpp)
-target_include_directories(btrmeta PRIVATE ${BTR_INCLUDE_DIR})
-target_link_libraries(btrmeta btrblocks gflags)
+# add_executable(btrmeta ${BTR_CONVERSION_DIR}/btrmeta.cpp)
+# target_include_directories(btrmeta PRIVATE ${BTR_INCLUDE_DIR})
+# target_link_libraries(btrmeta btrblocks gflags)
 
-add_executable(decompression-speed ${BTR_CONVERSION_DIR}/decompression-speed.cpp)
-target_include_directories(decompression-speed PRIVATE ${BTR_INCLUDE_DIR})
-target_link_libraries(decompression-speed btrblocks tbb gflags)
+# add_executable(decompression-speed ${BTR_CONVERSION_DIR}/decompression-speed.cpp)
+# target_include_directories(decompression-speed PRIVATE ${BTR_INCLUDE_DIR})
+# target_link_libraries(decompression-speed btrblocks tbb gflags)
 
-add_executable(decompression-speed-s3 ${BTR_CONVERSION_DIR}/decompression-speed-s3.cpp)
-target_include_directories(decompression-speed-s3 PRIVATE ${BTR_INCLUDE_DIR})
-target_link_libraries(decompression-speed-s3 btrblocks tbb gflags libaws-cpp-sdk-core libaws-cpp-sdk-s3-crt libaws-cpp-sdk-s3)
+# add_executable(decompression-speed-s3 ${BTR_CONVERSION_DIR}/decompression-speed-s3.cpp)
+# target_include_directories(decompression-speed-s3 PRIVATE ${BTR_INCLUDE_DIR})
+# target_link_libraries(decompression-speed-s3 btrblocks tbb gflags libaws-cpp-sdk-core libaws-cpp-sdk-s3-crt libaws-cpp-sdk-s3)
 
 # ---------------------------------------------------------------------------
 # Scripts
